596fe2a78c9b3481b3f5ed0ac9853e784c6f83e5
Fernando Avellar <favellar@yahoo.com.br>
2017-07-07 22:16:43 -0300

    Finalizado Feature Valor Final

--- scripts/relatorio.js
+++ scripts/relatorio.js
@@ -1,16 +1,19 @@
 'use strict'
 
 var comprasEncerradas = [];
+var valorTodasAsVendas = 0;
 
 retrieveFromLocalDBComprasEncerradas();
 gerarRelatorioDeVendas();
 
 function gerarRelatorioDeVendas() {
+  valorTodasAsVendas = 0;
   if (comprasEncerradas !== null) {
     let i = 0;
     for (i = 0; i < comprasEncerradas.length; i++) {
         insereCarrinhoNaTabela(comprasEncerradas[i]);
     }
+    geraLinhaFinalComValorTotalDasVendas();
   }
 }
 
@@ -28,16 +31,27 @@ function insereCarrinhoNaTabela(mapaDataCompraConteudoCarrinho) {
 function insereCompraNaTabela(compra, data) {
   let nomeLivro = getNomeLivroUsandoId(compra[0].id);
   let quantidade = compra[0].quantidade;
-  let valorTotal = 10 * quantidade; 
-  let tabela = document.querySelector("table");
+  let valorIndividualDaVenda = 10 * quantidade; 
+  valorTodasAsVendas = valorTodasAsVendas + valorIndividualDaVenda;
+  let tabela = document.querySelector("#tabelaRelatorio");
   let novaVenda = "<tr>" +
     "<td>" + data + "</td>" +
     "<td>" + quantidade + "</td>" +
     "<td>" + nomeLivro + "</td>" +
-    "<td>" + "R$ " + valorTotal + "</td>" +
+    "<td>" + "R$ " + valorIndividualDaVenda + "</td>" +
     "</tr>";
 
   tabela.innerHTML = tabela.innerHTML + novaVenda;
+}
+
+function geraLinhaFinalComValorTotalDasVendas() {
+  let tabelaFinal = document.querySelector("#tabelaValorFinal");
+
+  let valorFinal = "<tr>" +
+    "<td>" + "R$ " + valorTodasAsVendas + "</td>" +
+    "</tr>";
+
+  tabelaFinal.innerHTML = tabelaFinal.innerHTML + valorFinal;
 }
 
 function getNomeLivroUsandoId(id) {
